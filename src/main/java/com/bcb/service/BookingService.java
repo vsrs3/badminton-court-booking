7823dbbf824df000adc556d2ac4e3d32cea64529
Turtle <trannamanh.nat2004@gmail.com>
2026-01-25 23:39:28 +0700

    chore: remove example class

--- src/main/java/com/bcb/service/BookingService.java
+++
@@ -1,8 +0,0 @@
-package com.bcb.service;
-
-/**
- * Booking Service interface placeholder
- */
-public interface BookingService {
-    // Placeholder for booking service methods
-}
--- src/main/java/com/bcb/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.bcb.service;
-
-/**
- * User Service interface placeholder
- */
-public interface UserService {
-    // Placeholder for user service methods
-}
